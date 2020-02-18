dc96b58035d1532fd482cc05d88aaef9673715e6
Hayden Metsky <hmetsky@gmail.com>
2020-02-18 18:33:18 -0500

    Adjust design parameters for SARS

--- designs/coronaviridae/sars/run.sh
+++ designs/coronaviridae/sars/run.sh
@@ -18,4 +18,11 @@ USE_FASTA="in/use-fasta.tsv"
 # SARS species
 SPECIFICITY_IGNORE="in/specificity-ignore.tsv"
 
+# For these designs, increase specificity and the number
+# of output designs
+ARG_GM="0"
+ARG_IDM="5"
+ARG_IDFRAC="0"
+ARG_BESTNTARGETS="200"
+
 run-adapt complete-targets auto-from-file $IN_TSV $OUT_DIR -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m $ARG_IDM --id-frac $ARG_IDFRAC --id-method shard --require-flanking3 H --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds $ARG_PRIMER_GC_LO $ARG_PRIMER_GC_HI --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $PREDICTIVE_MODEL --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --only-design-for $ONLY_DESIGN_FOR --use-fasta $USE_FASTA --taxa-to-ignore-for-specificity $SPECIFICITY_IGNORE --write-input-seqs --write-input-aln --ncbi-api-key $NCBI_API_KEY --verbose
