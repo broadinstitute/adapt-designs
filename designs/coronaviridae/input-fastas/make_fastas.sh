2f1a5b3fc8e56269cdaa14273e37cbf856fa28be
Hayden Metsky <hmetsky@gmail.com>
2020-03-28 22:07:05 -0400

    Update FASTA files for SARS-related species groupings
    This updates make_fastas.sh to make new taxonomic groupings. The main
    new ones are SARS-CoV-1-related (consisting of SARS-CoV-1 and the
    closely related SARS-like sequences) and SARS-CoV-2-related (consisting
    of SARS-CoV-2 and the closely related SARS-like sequences).
    
    This commit also includes the output of running make_fastas.sh. Notably,
    it incorporates new SARS-CoV-2 sequences from GISAID (up to 2020-03-28).

--- designs/coronaviridae/input-fastas/make_fastas.sh
+++ designs/coronaviridae/input-fastas/make_fastas.sh
@@ -3,37 +3,58 @@
 # Combine accession lists for NCBI and FASTA sequences from GISAID to
 # create FASTAs for different taxonomic groupings.
 
+# The SARS-CoV-1-related and SARS-CoV-2-related groupings as are
+# defined in Fig. 1 of https://www.nature.com/articles/s41586-020-2169-0
+# They each include SARS-CoV-1 and SARS-CoV-2, as well as a subset of
+# SARS-like sequences that are closely related to SARS-CoV-1 or
+# SARS-CoV-2.
+
 # Load custom environment and variables; needed for conda
 # loading for ADAPT
 source ../../../scripts/run-adapt/custom-env/load_custom_env.sh
 
-GISAID_DL_DATE="20200223"
+GISAID_DL_DATE="20200328"
 
 # Delete fastas-for-design/ files already made
 rm -f fastas-for-design/*
 
 # Download sequences for NCBI accessions.
 python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-CoV-1.acc.txt > /tmp/NCBI_SARS-CoV-1.fasta
-python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.acc.txt > /tmp/NCBI_SARS-like.fasta
+python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.SARS-CoV-1_related.acc.txt > /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta
+python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.SARS-CoV-2_related.acc.txt > /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta
+python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.other.acc.txt > /tmp/NCBI_SARS-like.other.fasta
 
 # Make the SARS-CoV-2 FASTA; this is directly from GISAID
 cp in/GISAID_SARS-CoV-2.${GISAID_DL_DATE}.fasta fastas-for-design/SARS-CoV-2.fasta
 
-# Make the SARS-like FASTA, combining GISAID and NCBI
-cat /tmp/NCBI_SARS-like.fasta in/GISAID_SARS-like.${GISAID_DL_DATE}.fasta > fastas-for-design/SARS-like.fasta
+# Make the SARS-CoV-2-related FASTA, combining GISAID and NCBI
+# This includes SARS-CoV-2 and SARS-like sequences that are closely related
+# All SARS-like from GISAID is SARS-CoV-2-related
+cat /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta in/GISAID_SARS-like.${GISAID_DL_DATE}.fasta in/GISAID_SARS-CoV-2.${GISAID_DL_DATE}.fasta > fastas-for-design/SARS-CoV-2-related.fasta
 
 # Make the SARS-CoV-1 FASTA; this is directly from NCBI
 cp /tmp/NCBI_SARS-CoV-1.fasta fastas-for-design/SARS-CoV-1.fasta
+
+# Make the SARS-CoV-1-related FASTA, from NCBI
+# This includes SARS-CoV-1 and SARS-like sequences that are closely related
+cat /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta /tmp/NCBI_SARS-CoV-1.fasta > fastas-for-design/SARS-CoV-1-related.fasta
+
+# Make a full SARS-like FASTA
+cat /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta /tmp/NCBI_SARS-like.other.fasta in/GISAID_SARS-like.${GISAID_DL_DATE}.fasta > fastas-for-design/SARS-like.fasta
 
 # Make a complete SARS FASTA combining the disjoint groupings above
 cat fastas-for-design/SARS-CoV-1.fasta fastas-for-design/SARS-like.fasta fastas-for-design/SARS-CoV-2.fasta > fastas-for-design/SARS-all.fasta
 
 # gzip files
 gzip fastas-for-design/SARS-CoV-2.fasta
-gzip fastas-for-design/SARS-like.fasta
+gzip fastas-for-design/SARS-CoV-2-related.fasta
 gzip fastas-for-design/SARS-CoV-1.fasta
+gzip fastas-for-design/SARS-CoV-1-related.fasta
+gzip fastas-for-design/SARS-like.fasta
 gzip fastas-for-design/SARS-all.fasta
 
 # Remove tmp files
 rm /tmp/NCBI_SARS-CoV-1.fasta
-rm /tmp/NCBI_SARS-like.fasta
+rm /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta
+rm /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta
+rm /tmp/NCBI_SARS-like.other.fasta
