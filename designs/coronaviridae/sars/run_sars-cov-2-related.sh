244b102f6ff3f4c6968df99e366597856c0228db
Hayden Metsky <hmetsky@gmail.com>
2020-04-07 11:02:35 -0400

    Update SARS-related species designs and add two taxonomies within this species
    This updates designs for the SARS-related species, SARS-CoV-1,
    SARS-CoV-2, and SARS-like using the most recent data from GISAID.
    Additionally, it adds two taxonomies within the species --
    SARS-CoV-2-related and SARS-CoV-1-related -- as defined in Fig. 1 of
    https://www.nature.com/articles/s41586-020-2169-0.

---
+++ designs/coronaviridae/sars/run_sars-cov-2-related.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ../../../scripts/run-adapt/run_common.sh
+
+# Set input
+IN_TSV="in/taxa.tsv"
+
+# Use FASTAs for SARS and its subgroupings
+USE_FASTA="in/use-fasta.tsv"
+
+# The taxonomies overlap, which complicates specificity
+# Do not be specific against within-SARS strains when designing
+# for the SARS species; likewise, when designing for
+# within-SARS strains, do not be specific against the whole
+# SARS species
+SPECIFICITY_IGNORE="in/specificity-ignore.tsv"
+
+# For all of these designs, increase the number of output designs
+ARG_BESTNTARGETS="200"
+
+# Design only for SARS-CoV-2-related, using custom parameters
+# Thresholds on GP and PP need to be increased because the vast
+# majority of this is SARS-CoV-2 (1775 of 1784), but we want to cover non-SARS-CoV-2
+# sequences too
+ONLY_DESIGN_FOR="in/only-design.sars-cov-2-related.tsv"
+ARG_GM="1"
+ARG_PP="0.9990"
+ARG_GP="0.9995"
+ARG_ACTIVITYTHRES="-0.90"
+ARG_IDM="4"
+ARG_IDFRAC="0"
+run-adapt complete-targets auto-from-file $IN_TSV $OUT_DIR -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m $ARG_IDM --id-frac $ARG_IDFRAC --id-method shard --require-flanking3 H --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds $ARG_PRIMER_GC_LO $ARG_PRIMER_GC_HI --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $PREDICTIVE_MODEL --predict-activity-thres $ARG_ACTIVITYTHRES --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --only-design-for $ONLY_DESIGN_FOR --use-fasta $USE_FASTA --taxa-to-ignore-for-specificity $SPECIFICITY_IGNORE --write-input-seqs --write-input-aln --ncbi-api-key $NCBI_API_KEY --verbose
+mv out/design.out.gz out/design.sars-cov-2-related.out.gz
