493acc5a46c7f7f5b650ade8c6c280662cf1c96d
Hayden Metsky <hmetsky@gmail.com>
2020-04-30 20:42:13 -0400

    Update SARS-CoV-2 genomes and add a new SARS-CoV-2_loose-surrounding FASTA
    SARS-CoV-2_loose-surrounding.fasta.gz contains everything in the
    SARS-related species, except for SARS-CoV-2 and two sequences (RaTG13
    and a pangolin sequence) that are very closely related to SARS-CoV-2.
    This lets us design for SARS-CoV-2 in a way where we can be specific but
    without constraining the design too much (because those two sequences
    can really constrain the design).

--- designs/coronaviridae/input-fastas/README.txt
+++ designs/coronaviridae/input-fastas/README.txt
@@ -2,5 +2,10 @@ This contains accession lists from NCBI and sequences from GISAID for
 different taxonomic groupings. They are inside in/
 I have stored locally information about curating these lists.
 
+The '_trimmed' files simply remove two sequences (RaTG13, as an NCBI
+accession; and GD/P1L (Guandong pangolin sequence) from GISIAD). These
+are extremely similar to SARS-CoV-2 and not having them lets us make a
+relaxed SARS-CoV-2 design that is not as constrained in specificity.
+
 The script make_fastas.sh groups these to create different FASTA files
 for different taxonomic groupings, placed in fastas-for-design/
--- designs/coronaviridae/input-fastas/make_fastas.sh
+++ designs/coronaviridae/input-fastas/make_fastas.sh
@@ -13,7 +13,7 @@
 # loading for ADAPT
 source ../../../scripts/run-adapt/custom-env/load_custom_env.sh
 
-GISAID_DL_DATE="20200328"
+GISAID_DL_DATE="20200428"
 
 # Delete fastas-for-design/ files already made
 rm -f fastas-for-design/*
@@ -22,6 +22,7 @@ rm -f fastas-for-design/*
 python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-CoV-1.acc.txt > /tmp/NCBI_SARS-CoV-1.fasta
 python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.SARS-CoV-1_related.acc.txt > /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta
 python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.SARS-CoV-2_related.acc.txt > /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta
+python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.SARS-CoV-2_related_trimmed.acc.txt > /tmp/NCBI_SARS-like.SARS-CoV-2_related_trimmed.fasta
 python ../../../scripts/download_fasta_for_accessions.py in/NCBI_SARS-like.other.acc.txt > /tmp/NCBI_SARS-like.other.fasta
 
 # Make the SARS-CoV-2 FASTA; this is directly from GISAID
@@ -31,6 +32,9 @@ cp in/GISAID_SARS-CoV-2.${GISAID_DL_DATE}.fasta fastas-for-design/SARS-CoV-2.fas
 # This includes SARS-CoV-2 and SARS-like sequences that are closely related
 # All SARS-like from GISAID is SARS-CoV-2-related
 cat /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta in/GISAID_SARS-like.${GISAID_DL_DATE}.fasta in/GISAID_SARS-CoV-2.${GISAID_DL_DATE}.fasta > fastas-for-design/SARS-CoV-2-related.fasta
+
+# Make a FASTA that is like SARS-CoV-2-related, except without SARS-CoV-2 and a few sequences highly similar to SARS-CoV-2
+cat /tmp/NCBI_SARS-like.SARS-CoV-2_related_trimmed.fasta in/GISAID_SARS-like_trimmed.${GISAID_DL_DATE}.fasta > /tmp/SARS-CoV-2-related_without_SARS-CoV-2_and_surrounding.fasta
 
 # Make the SARS-CoV-1 FASTA; this is directly from NCBI
 cp /tmp/NCBI_SARS-CoV-1.fasta fastas-for-design/SARS-CoV-1.fasta
@@ -42,6 +46,9 @@ cat /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta /tmp/NCBI_SARS-CoV-1.fasta > fa
 # Make a full SARS-like FASTA
 cat /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta /tmp/NCBI_SARS-like.other.fasta in/GISAID_SARS-like.${GISAID_DL_DATE}.fasta > fastas-for-design/SARS-like.fasta
 
+# Make a SARS FASTA that has everything except SARS-CoV-2 and sequences that are highly related to SARS-CoV-2 (RaTG13 and a pangolin sequence, GD/P1L)
+cat fastas-for-design/SARS-CoV-1.fasta /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta /tmp/NCBI_SARS-like.other.fasta /tmp/SARS-CoV-2-related_without_SARS-CoV-2_and_surrounding.fasta > fastas-for-design/SARS-CoV-2_loose-surrounding.fasta
+
 # Make a complete SARS FASTA combining the disjoint groupings above
 cat fastas-for-design/SARS-CoV-1.fasta fastas-for-design/SARS-like.fasta fastas-for-design/SARS-CoV-2.fasta > fastas-for-design/SARS-all.fasta
 
@@ -52,9 +59,11 @@ gzip fastas-for-design/SARS-CoV-1.fasta
 gzip fastas-for-design/SARS-CoV-1-related.fasta
 gzip fastas-for-design/SARS-like.fasta
 gzip fastas-for-design/SARS-all.fasta
+gzip fastas-for-design/SARS-CoV-2_loose-surrounding.fasta
 
 # Remove tmp files
 rm /tmp/NCBI_SARS-CoV-1.fasta
 rm /tmp/NCBI_SARS-like.SARS-CoV-1_related.fasta
 rm /tmp/NCBI_SARS-like.SARS-CoV-2_related.fasta
+rm /tmp/SARS-CoV-2-related_without_SARS-CoV-2_and_surrounding.fasta
 rm /tmp/NCBI_SARS-like.other.fasta
