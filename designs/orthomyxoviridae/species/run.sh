cba4182cb6235e30fb75c406c770684e57dcd909
Hayden Metsky <hmetsky@gmail.com>
2020-06-17 13:26:48 -0400

    Add updated designs that maximize expected activity

--- designs/orthomyxoviridae/species/run.sh
+++ designs/orthomyxoviridae/species/run.sh
@@ -8,13 +8,12 @@ source ../../../scripts/run-adapt/run_common.sh
 IN_TSV="in/taxa.tsv"
 
 # Relax some parameters; with the default, there are no IAV designs
-ARG_GP="0.98"
-ARG_PP="0.95"
+ARG_PP="0.90"
 
 # Make accessions input file
 ACCESSIONS_TO_USE="in/accessions-to-use.tsv"
-cat ../../inputs/IAV.Hany-Nany.segment2.acc.txt | grep . | awk '{print "11320\t2\t"$1}' > $ACCESSIONS_TO_USE
-cat ../../inputs/IBV.segment1.acc.txt | grep . | awk '{print "11520\t1\t"$1}' >> $ACCESSIONS_TO_USE
+cat ../accession-lists/IAV.Hany-Nany.segment2.acc.txt | grep . | awk '{print "11320\t2\t"$1}' > $ACCESSIONS_TO_USE
+cat ../accession-lists/IBV.segment1.acc.txt | grep . | awk '{print "11520\t1\t"$1}' >> $ACCESSIONS_TO_USE
 
 # Run the design
-run-adapt design.py complete-targets auto-from-file $IN_TSV $OUT_DIR -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m $ARG_IDM --id-frac $ARG_IDFRAC --id-method shard --require-flanking3 H --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds $ARG_PRIMER_GC_LO $ARG_PRIMER_GC_HI --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $ARG_PREDICTIVE_MODELS --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --use-accessions $ACCESSIONS_TO_USE --write-input-seqs --write-input-aln --ncbi-api-key $NCBI_API_KEY --verbose
+run-adapt design complete-targets auto-from-file $IN_TSV $OUT_DIR --obj maximize-activity --soft-guide-constraint $ARG_SOFTGUIDECONSTRAINT --hard-guide-constraint $ARG_HARDGUIDECONSTRAINT --penalty-strength $ARG_PENALTYSTRENGTH --maximization-algorithm $ARG_MAXIMIZATIONALGORITHM -gl $ARG_GL -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m $ARG_IDM --id-frac $ARG_IDFRAC --id-method shard --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds $ARG_PRIMER_GC_LO $ARG_PRIMER_GC_HI --max-target-length $ARG_MAXTARGETLENGTH --obj-fn-weights $ARG_OBJFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $ARG_PREDICTIVE_MODELS --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --use-accessions $ACCESSIONS_TO_USE --write-input-seqs --write-input-aln --ncbi-api-key $NCBI_API_KEY --verbose
--- designs/picornaviridae/enterovirus-species-entero/run.sh
+++ designs/picornaviridae/enterovirus-species-entero/run.sh
@@ -9,25 +9,13 @@ ONLY_DESIGN_FOR="in/only-design.tsv"
 # These have a lot of diversity, especially the enteroviruses
 # Relax some parameters; with the default, there are very few
 # options for a several of the species
-ARG_GP="0.90"
 ARG_PM="3"
-ARG_PP="0.95"
+ARG_PP="0.85"
 ARG_PRIMER_GC_LO="0.35"
 ARG_PRIMER_GC_HI="0.65"
 ARG_MAXPRIMERSATSITE="10"
 ARG_IDM="3"
 ARG_IDFRAC="0.10"
-ARG_BESTNTARGETS="7"
+ARG_BESTNTARGETS="10"
 
-# To handle diversity, also cluster sequences (default value
-# does not cluster -- or, more specifically, always yields
-# one cluster)
-CLUSTER_THRESHOLD="1.0"
-
-# Set a non-default activity threshold, lower than the default
-# There is so much diversity in this family (including within-species),
-# and that adds to the constraints; lowering the activity threshold
-# frees up more designs
-ARG_ACTIVITYTHRES="0.577467 2.0" # default classification threshold and decreased regression threshold
-
-run-adapt complete-targets auto-from-file $IN_TSV $OUT_DIR -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m $ARG_IDM --id-frac $ARG_IDFRAC --id-method shard --require-flanking3 H --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds $ARG_PRIMER_GC_LO $ARG_PRIMER_GC_HI --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $ARG_PREDICTIVE_MODELS --predict-activity-thres $ARG_ACTIVITYTHRES --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --only-design-for $ONLY_DESIGN_FOR --write-input-seqs --write-input-aln --ncbi-api-key $NCBI_API_KEY --verbose
+run-adapt design complete-targets auto-from-file $IN_TSV $OUT_DIR --obj maximize-activity --soft-guide-constraint $ARG_SOFTGUIDECONSTRAINT --hard-guide-constraint $ARG_HARDGUIDECONSTRAINT --penalty-strength $ARG_PENALTYSTRENGTH --maximization-algorithm $ARG_MAXIMIZATIONALGORITHM -gl $ARG_GL -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m $ARG_IDM --id-frac $ARG_IDFRAC --id-method shard --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds $ARG_PRIMER_GC_LO $ARG_PRIMER_GC_HI --max-target-length $ARG_MAXTARGETLENGTH --obj-fn-weights $ARG_OBJFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $ARG_PREDICTIVE_MODELS --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --only-design-for $ONLY_DESIGN_FOR --write-input-seqs --write-input-aln --ncbi-api-key $NCBI_API_KEY --verbose
